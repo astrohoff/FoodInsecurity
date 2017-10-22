9978660e2d1e625eb2f0ffbdedf3bfd89918b486
Alec Hoffmann <astrohoff@ufl.edu>
2017-10-22 11:22:40 -0400

    Implemented health and movement cost. Implemented food. Need to make collider center itself on player head, and make sure food collision is being registered.

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour {
+    public float health = 2;
+
+    void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.tag == "Player"){
+            collision.gameObject.GetComponent<Player>().ChangeHealth(health);
+            Destroy(gameObject);
+        }
+    }
+}
