e3ce1fd6c02cab2e339e1c2c4236fd9fe1bb7eef
selassie-eye <ollivierre.khari@gmail.com>
2017-10-27 20:17:08 -0400

    Initial Scene Setup + Basic Money Implementation

---
+++ Economy/Assets/Scripts/vend.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vend : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTriggerStay (Collider other) {
+		if (other.tag == "Player") {
+			BroadcastMessage ("vendProx", 0, SendMessageOptions.DontRequireReceiver);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
