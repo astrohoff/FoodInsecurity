563c369f0b870f5b90b556599e445ee5225c91bc
Alec Hoffmann <astrohoff@ufl.edu>
2017-11-03 19:23:46 -0400

    Added ability to eat food by clicking on it (for non-vr mode).

--- Assets/Scripts/MousePointer.cs
+++ Assets/Scripts/MousePointer.cs
@@ -6,11 +6,30 @@ public class MousePointer : MonoBehaviour
     private GameObject focus;
 
     public void Update(){
-        
+        UpdateFocus();
+        TryFoodClick();
     }
 
     private void UpdateFocus(){
-        Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray pointRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(pointRay, out hitInfo))
+        {
+            focus = hitInfo.collider.gameObject;
+        }
+        else
+        {
+            focus = null;
+        }
+    }
 
+    private void TryFoodClick(){
+        if(Input.GetMouseButtonDown(0)){
+            if(focus != null && focus.GetComponent<Food>() != null){
+                GetComponent<Player>().ChangeHealth(focus.GetComponent<Food>().health);
+                Destroy(focus);
+                focus = null;
+            }
+        }
     }
 }
