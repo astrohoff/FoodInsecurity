bf9a9074cf1419d0345d37fe392408e781f14d1f
Alec Hoffmann <astrohoff@ufl.edu>
2017-11-03 19:33:47 -0400

    Started adding Touch grabbing.

---
+++ Assets/Scripts/GrabControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabControl : MonoBehaviour {
+    public bool isLeftHand = true;
+
+    private GameObject grabTarget;
+
+    void Update(){
+        
+
+    }
+
+    private void GrabUpdate(){
+        if(isLeftHand && OVRInput.GetDown(OVRInput.RawButton.LHandTrigger) ||
+            !isLeftHand && OVRInput.GetDown(OVRInput.RawButton.RHandTrigger))
+        {
+            if(grabTarget != null){
+                grabTarget.transform.SetParent(transform);
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider collider){
+        Debug.Log("Hand triggered");
+        if(collider.GetComponent<Food>() != null){
+            grabTarget = collider.gameObject;
+        }
+    }
+}
