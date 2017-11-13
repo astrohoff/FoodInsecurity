3981523e5391d316b14f224006e1e6d21d246e05
Unknown <ollivierre.khari@gmail.com>
2017-11-12 19:54:31 -0500

    Updated Economy
    Added scripts to facilitate commercial elements within the game, specifically buying and selling food.

---
+++ Economy-2.0/Assets/PlayerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+    private Inventory inventory;
+
+	// For testing purposes only
+	// ------------------------------------------
+	private VendorEx vendor;
+	private AbstractFood.foodEnum cur;
+	// ------------------------------------------
+
+
+	// Use this for initialization
+	void Start () {
+        inventory = new Inventory();
+		vendor = GameObject.FindWithTag("VendorTest").GetComponent<VendorEx>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// For testing purposes only
+		// ------------------------------------------
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			cur = vendor.stockCycleUp();
+			Debug.Log(cur + " selected.");
+		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			cur = vendor.stockCycleDown();
+			Debug.Log(cur + " selected.");
+		} else if (Input.GetKeyDown(KeyCode.Return)) {
+			Debug.Log("Buying " + cur + "...");
+			vendor.Vend(cur);
+		}
+		// ------------------------------------------
+
+	}
+
+    public void BuyFood(Food food) { inventory.BuyFood(food); }
+}
